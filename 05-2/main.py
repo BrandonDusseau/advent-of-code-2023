0b855762b07f5aa193732f9f2e978a20846d0e2e
Brandon Dusseau <3901864+BrandonDusseau@users.noreply.github.com>
2023-12-05 18:27:39 -0500

    Add initial attempts for day 5

---
+++ 05-2/main.py
@@ -0,0 +1,61 @@
+import os
+import math
+
+with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
+    lines = f.readlines()
+
+seeds = []
+seed_soil_map = []
+soil_fertilizer_map = []
+fertilizer_water_map = []
+water_light_map = []
+light_temperature_map = []
+temperature_humidity_map = []
+humidity_location_map = []
+min_location = math.inf
+
+def parse_mapping(start_index, the_map):
+    line_index < len(lines)
+    index = start_index
+    while index < len(lines) and lines[index].strip() != "":
+        split_line = lines[index].strip().split()
+        the_map.append((int(split_line[0]), int(split_line[1]), int(split_line[2])))
+        index += 1
+    return index
+
+def get_destination(source, map):
+    for mapping in map:
+        (map_dest, map_source, map_range) = mapping
+        if source in range(map_source, map_source + map_range):
+            return map_dest + (source - map_source)
+    return source
+
+# Parse the input file.
+line_index = 0
+seed_data = lines[line_index].strip().split(":")[1].split()
+for i in range(0, len(seed_data), 2):
+    seeds.append((int(seed_data[i]), int(seed_data[i + 1])))
+line_index += 3
+line_index = parse_mapping(line_index, seed_soil_map) + 2
+line_index = parse_mapping(line_index, soil_fertilizer_map) + 2
+line_index = parse_mapping(line_index, fertilizer_water_map) + 2
+line_index = parse_mapping(line_index, water_light_map) + 2
+line_index = parse_mapping(line_index, light_temperature_map) + 2
+line_index = parse_mapping(line_index, temperature_humidity_map) + 2
+parse_mapping(line_index, humidity_location_map)
+
+# Time to follow the maps.
+for seed in seeds:
+    for seed_num in range(seed[0], seed[0] + seed[1]):
+        soil_num = get_destination(seed_num, seed_soil_map)
+        fertilizer_num = get_destination(soil_num, soil_fertilizer_map)
+        water_num = get_destination(fertilizer_num, fertilizer_water_map)
+        light_num = get_destination(water_num, water_light_map)
+        temp_num = get_destination(light_num, light_temperature_map)
+        humid_num = get_destination(temp_num, temperature_humidity_map)
+        location_num = get_destination(humid_num, humidity_location_map)
+
+        if location_num < min_location:
+            min_location = location_num
+
+print(min_location)
