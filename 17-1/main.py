d7c3d10adfd8be09629920897ced47dbfeec17c8
Brandon Dusseau <3901864+BrandonDusseau@users.noreply.github.com>
2023-12-22 00:59:06 -0500

    More attempts for day 17

--- 17-1/main.py
+++ 17-1/main.py
@@ -82,6 +82,12 @@ def validate_path(path):
             return False
     return True
 
+def find_neighbor_in_direction(block, direction):
+    for neighbor_key in block.neighbors:
+        neighbor = blocks[neighbor_key]
+        if get_direction(block, neighbor) == direction:
+            return neighbor
+
 def delete_neighbor_in_direction(block, direction):
     for neighbor_key in block.neighbors:
         neighbor = blocks[neighbor_key]
@@ -111,6 +117,63 @@ def restore_neighbors(pairs):
         blocks[pair[0]].neighbors.append(pair[1])
         blocks[pair[1]].neighbors.append(pair[0])
 
+
+def find_path(blocks, start, goal, current_path, directions_taken, cost_so_far):
+    if start == goal:
+        return (current_path, cost_so_far)
+
+    current_block = blocks[start]
+    print(f"Currently at {start} having amassed a cost of {cost_so_far}")
+
+    if len(current_path) > len(blocks) // 2:
+        print(f"Moving back up as we've exceeded the max distance")
+        return None
+
+    forbidden_directions = set()
+
+    # Can't travel more than three nodes in a given direction
+    if len(directions_taken) >= 3 and len(set(directions_taken[-3:])):
+        forbidden_directions.add(directions_taken[-1])
+
+    # Can't travel backwards
+    if len(current_path) > 1:
+        forbidden_directions.add(get_direction(current_block, blocks[current_path[-1]]))
+
+    possible_paths = []
+    new_cost = cost_so_far + current_block.heat_loss
+    for neighbor_key in current_block.neighbors:
+        # Technically we can cross back into the path, but that's counterproductive.
+        if neighbor_key in current_path:
+            print(f"Can't go back onto the path!")
+            continue
+
+        # Can't go to neighbors that aren't allowed.
+        neighbor_direction = get_direction(current_block, blocks[neighbor_key])
+        if neighbor_direction in forbidden_directions:
+            print(f"Can't go this way!")
+            continue
+
+        new_directions_taken = directions_taken + [neighbor_direction]
+        new_path = current_path + [neighbor_key]
+
+        result = find_path(blocks, neighbor_key, goal, new_path, new_directions_taken, new_cost)
+        if result is not None:
+            possible_paths.append(result)
+
+    if len(possible_paths) == 0:
+        print("Moving back up as no possible paths were found on this route")
+        return None
+
+    min_path = None
+    min_cost = math.inf
+    for path, cost in possible_paths:
+        if cost < min_cost:
+            min_path = path
+            min_cost = cost
+
+    print(f"Found path to goal with cost {min_cost}")
+    return (min_path, min_cost)
+
 blocks = {}
 num_rows = 0
 num_cols = 0
@@ -136,36 +199,5 @@ for loc, block in blocks.items():
     if block.col < num_cols - 1:
         block.neighbors.append((block.row, block.col + 1))
 
-current_position = blocks[(0, 0)]
-shortest_path = [current_position]
-goal = blocks[(num_rows - 1, num_cols - 1)]
-last_three_directions = []
-while current_position != goal:
-    deleted_neighbors = []
-
-    # We can't go backwards.
-    if len(last_three_directions) != 0:
-        deleted_neighbors.append(delete_reverse_neighbor(current_position, last_three_directions[-1]))
-
-    # If we've traveled the same direction three times in a row, we can't proceed in that direction again.
-    if len(last_three_directions) == 3 and len(set(last_three_directions)) == 1:
-        deleted_neighbors.append(delete_neighbor_in_direction(current_position, last_three_directions[-1]))
-
-    # Get the shortest path from here.
-    next_shortest_path = get_shortest_path(blocks, current_position, goal)
-    next_position = next_shortest_path[1]
-    shortest_path.append(next_position)
-
-    if len(last_three_directions) == 3:
-        last_three_directions.pop(0)
-    last_three_directions.append(get_direction(current_position, next_position))
-    print(f"Moving {last_three_directions[-1]}")
-    print()
-
-    # Put back the neighbors we removed.
-    restore_neighbors(deleted_neighbors)
-
-    current_position = next_position
-
-pprint(shortest_path)
-print(sum([x.heat_loss for x in shortest_path]) - shortest_path[0].heat_loss)
+result = find_path(blocks, (0, 0), (num_rows - 1, num_cols - 1), [(0, 0)], [], 0)
+pprint(result)
